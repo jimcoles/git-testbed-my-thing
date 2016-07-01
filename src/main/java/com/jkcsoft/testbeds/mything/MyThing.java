089a00f646a1d2f9c1ed482ae071198f88cd354c
Jim Coles <jameskcoles@gmail.com>
2016-06-30 19:06:27 -0500

    bug-1 fix

--- src/main/java/com/jkcsoft/testbeds/mything/MyThing.java
+++ src/main/java/com/jkcsoft/testbeds/mything/MyThing.java
@@ -4,4 +4,8 @@ package com.jkcsoft.testbeds.mything;
  * Created by jcoles on 6/29/16.
  */
 public class MyThing {
+    // fix for bug-1
+    public static void main(String[] args) {
+        System.out.println("fix bug-1");
+    }
 }
