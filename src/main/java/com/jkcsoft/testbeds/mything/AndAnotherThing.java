9633e31567c976a4ebe954b560f0388cbf08f714
Jim Coles <jameskcoles@gmail.com>
2016-07-05 16:56:47 -0500

    a new thing and a rename

---
+++ src/main/java/com/jkcsoft/testbeds/mything/AndAnotherThing.java
@@ -0,0 +1,7 @@
+package com.jkcsoft.testbeds.mything;
+
+/**
+ * Created by jcoles on 7/5/16.
+ */
+public class AndAnotherThing {
+}
--- src/main/java/com/jkcsoft/testbeds/mything/RenamedMyThing.java
+++ src/main/java/com/jkcsoft/testbeds/mything/RenamedMyThing.java
@@ -3,7 +3,7 @@ package com.jkcsoft.testbeds.mything;
 /**
  * Created by jcoles on 6/29/16.
  */
-public class MyThing {
+public class RenamedMyThing {
     // fix for bug-1
     public static void main(String[] args) {
         System.out.println("fix bug-1");
